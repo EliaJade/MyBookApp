643a6437b7efa6597a54ff83590bd09c1f975a7a
EliaJade <eliajadesmith23@gmail.com>
2025-04-08 13:31:03 +0200

    Added class Book

--- app/src/main/java/com/example/mybookapp/Book.kt
+++ app/src/main/java/com/example/mybookapp/Book.kt
@@ -10,39 +10,83 @@ data class Book (
     val id: String,
     val etag: String,
     val selfLink: String,
-    val volumeInfo: VolumeInfo
-){
-
-}
-data class VolumeInfo (
-    val title: String,
-    val description: String,
-    val authors: String,
-    val publisher: String,
-    val publishedDate: String,
-    val industryIdentifiers: IndustryIdentifiers,
-    val readingModes: ReadingModes,
-    val printType: String,
-    val maturityRating: String,
-    val allowAnonLogging: Boolean,
-    val contentVersion: String,
-    val language: String,
-    val previewLink: String,
-    val infoLink: String,
-    val canonicalVolumeLink: String,
-){
-
-}
-data class IndustryIdentifiers (
-    val type: String,
-    val identifier: Int,
+    val volumeInfo: VolumeInfo,
+    val saleInfo: SaleInfo,
+    val accessInfo: AccessInfo,
+    val searchInfo: SearchInfo
 
 ){
 
 }
-data class ReadingModes (
-    val text: Boolean,
-    val image: Boolean
-){
+    data class AccessInfo (
+        val country: String,
+        val viewability: String,
+        val embeddable: Boolean,
+        val publicDomain: Boolean,
+        val textToSpeechPermission: String, //possible Boolean
+        val epub: Epub,
+        val pdf: Pdf,
+        val webReaderLink: String,
+        val accessViewStatus: String,
+        val quoteSharingAllowed: Boolean
+    ){
 
-}
+    }
+        data class Epub(
+            val isAvailable: Boolean
+        ){
+        }
+        data class Pdf(
+            val isAvailable: Boolean
+        ){
+        }
+
+    data class SaleInfo (
+        val country: String,
+        val saleability: String, //possible boolean
+        val isEbook: Boolean
+
+    ){
+
+    }
+    data class SearchInfo (
+        val textSnippet: String
+    ){
+
+    }
+    data class VolumeInfo (
+        val title: String,
+        val subtitle: String? = null,
+        val description: String,
+        val authors: String,
+        val publisher: String,
+        val publishedDate: String,
+        val industryIdentifiers: IndustryIdentifiers,
+        val readingModes: ReadingModes,
+        val pageCount: Int? = null,
+        val categories: String? = null,
+        val printType: String,
+        val maturityRating: String, //possible boolean
+        val allowAnonLogging: Boolean,
+        val contentVersion: String,
+        val imageLinks: String? = null,
+        val language: String,
+        val previewLink: String,
+        val infoLink: String,
+        val canonicalVolumeLink: String,
+    ){
+
+    }
+        data class IndustryIdentifiers (
+            val type: String,
+            val identifier: Int,
+
+        ){
+
+        }
+        data class ReadingModes (
+            val text: Boolean,
+            val image: Boolean
+        ){
+
+        }
