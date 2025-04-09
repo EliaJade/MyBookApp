42cd31ec0c936694dcd813ae32a621131e20ec0f
EliaJade <eliajadesmith23@gmail.com>
2025-04-10 00:34:20 +0200

    WIP: binding and recyclerView

--- app/src/main/java/com/example/mybookapp/MainActivity.kt
+++ app/src/main/java/com/example/mybookapp/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.mybookapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.mybookapp.databinding.ActivityMainBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -13,15 +16,28 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter: BookAdapter
+    lateinit var binding: ActivityMainBinding
+
+    var bookList: List<Book> = listOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.recyclerView.adapter = adapter
+
+        binding.recyclerView.layoutManager = GridLayoutManager(this, 2)
 
         getRetrofit()
     }
