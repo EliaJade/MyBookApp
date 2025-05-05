5d5e26b0eff072b7c70e98fa9e1da9bbdac4038b
EliaJade <eliajadesmith23@gmail.com>
2025-05-05 20:23:47 +0200

    added color to status icons, still WIP functionality on tabBars

--- app/src/main/java/com/example/mybookapp/data/Status.kt
+++ app/src/main/java/com/example/mybookapp/data/Status.kt
@@ -2,8 +2,8 @@ package com.example.mybookapp.data
 
 import com.example.mybookapp.R
 
-enum class Status (val title: Int, val icon: Int, ) {
-    WANT_TO_READ(R.string.status_want_to_read, R.drawable.ic_status_want_to_read),
-    READING(R.string.status_reading, R.drawable.ic_status_reading),
-    READ(R.string.status_read, R.drawable.ic_status_read)
+enum class Status (val title: Int, val icon: Int, val color: Int) {
+    WANT_TO_READ(R.string.status_want_to_read, R.drawable.ic_status_want_to_read, R.color.want_to_read),
+    READING(R.string.status_reading, R.drawable.ic_status_reading, R.color.reading),
+    READ(R.string.status_read, R.drawable.ic_status_read, R.color.read)
 }
